90b76d54978f60f1237390bcdabe7117549cf9d3
Manraj Singh <manraj.saini123@gmail.com>
2023-10-26 09:43:53 +0530

    functions passed as props

--- src/Components/NoteComponents/Notes.js
+++ src/Components/NoteComponents/Notes.js
@@ -1,18 +1,55 @@
 import React from 'react'
 import Note from './Note'
+import CreateNote from './CreateNote';
+
 import "../css/Note.css"
+
+import {v4 as uuid} from "uuid";
 
 import { useState, useEffect } from "react";
 
 const Notes = () => {
-  return (
-    <div className='notes'>
-        <Note />
-        <Note />
-        <Note />
-        <Note />
-    </div>
-  )
+
+    const [notes, setNotes] = useState([]);
+    const [inputText, setInputText] = useState("");
+
+    const textHandler = (e) => {
+        setInputText(e.target.value);
+    }
+
+    const saveHandler = () => {
+        setNotes((oldNotes) => [
+            ...oldNotes,
+            {
+                id: uuid(),
+                text: inputText
+            }
+        ]);
+
+        setInputText("");
+    }
+
+    const deleteHandler = (noteId) => {
+        const filteredNotes = notes.filter(
+            (note) => note.id !== noteId
+        );
+
+        setNotes(filteredNotes);
+    }
+
+    return (
+        <div className='notes'>
+            <Note />
+            <Note />
+            <Note />
+            <Note />
+            <CreateNote 
+                textHandler={textHandler}
+                saveHandler={saveHandler}
+                inputText={inputText}
+            />
+        </div>
+    )
 }
 
 export default Notes
